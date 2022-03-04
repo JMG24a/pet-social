c23cd90208bfcbb181fb8d59ac1b7828d835d3f6
JMG24a <jmg24a@gmail.com>
2022-03-04 13:39:05 -0400

    [ADD] webpack config

---
+++ webpack.config.dev.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+//const copyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.resolve(__dirname,"dist"),
+		publicPath: "/",
+		filename: "bundle.js"
+	},
+	resolve: {
+		extensions: ['.js','.jsx']
+	},
+	mode: 'development',
+	module:{
+		rules:[
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.html$/,
+				use:{
+					loader: 'html-loader'
+				}
+			},
+			{
+				test: /\.(png|svg)$/,
+				type: 'assect/resource'
+			}
+		]
+	},
+	plugins:[
+		new htmlWebpackPlugin({
+			template: "./public/index.html",
+			filename: "./index.html"
+		}),
+		/*new copyWebpackPlugin({
+			patterns:[
+				{
+					from: path.resolve(__dirname, "src/assets/icons"),
+					to: "assets/icons"
+				}
+			]
+		}),*/
+	],
+    devServer:{
+        static: path.join(__dirname, "dist"),
+        historyApiFallback: true,
+		port: 3005,
+        compress: true
+    }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const terserWebpackPlugin = require("terser-webpack-plugin")
+
+module.export = {
+	entry: "src/index.js",
+	output: {
+		path: path.resolve(__dirname,"dist"),
+		publicPath: "/",
+		filename: "bundle.js"
+	},
+	resolve: {
+		extensions: [".js",".jsx"]
+	},
+	mode: 'production',
+	module:{
+		rules:[
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader"
+				}
+			},
+			{
+				test: /\.html$/,
+				use:{
+					loader: "html-loader"
+				}
+			},
+			{
+				test: /\.(png|svg)$/,
+				type: "assect/resource"
+			}
+		]
+	},
+	plugins:[
+		new htmlWebpackPlugin({
+			template: "./public/index.html",
+			filename: "./index.html"
+		}),
+		/*new copyWebpackPlugin({
+			patterns:[
+				{
+					from: path.resolve(__dirname, "src/assets/icons"),
+					to: "assets/icons"
+				}
+			]
+		}),*/
+		new CleanWebpackPlugin()	
+	],
+	optimization: {
+		minimize: true,
+		minimizer:[
+			new terserWebpackPlugin()
+		]
+	}
+}
