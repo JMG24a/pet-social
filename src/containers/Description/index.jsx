1ef78d9f96026c5a1638b3ee7e0a748f7b00360a
JMG24a <jmg24a@gmail.com>
2022-03-08 11:22:26 -0400

    [ADD] container description

---
+++ src/containers/Description/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+//graphql
+import { useQuery } from "@apollo/client";
+import { getPhoto } from '../../context/getPhoto';
+//components
+import { PhotoCard } from '../../components/PhotoCard';
+import { Loading } from '../../components/Loading';
+
+function Description({id}){
+
+    const { 
+        loading, 
+        error, 
+        data 
+    } = useQuery(getPhoto,{ variables: { id: id } });
+
+    const centerLoading = {
+        position: "relative"
+    }
+
+    return(
+        <div style={centerLoading}> 
+            {!!loading && <Loading/>}
+            {!!error && !loading && <p>ERROR</p>}
+            {!loading && 
+            <div>
+                {
+                    console.log('Calequa', data.photo)
+                }
+                <PhotoCard {...data.photo}/>
+            </div>
+            }
+        </div>
+    )
+}
+
+export {Description}
