808a97ed624bd0a590a1659dac7a96a196d7a8f4
JMG24a <jmg24a@gmail.com>
2022-03-10 16:12:36 -0400

    [UPDATE] se agrego rutas protegidas en app.jsx

--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,44 +1,55 @@
-import React, { useContext } from "react";
-import { Context } from "../context/context";
+import React, { useContext, Suspense } from "react";
+import { Context } from "../context";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 //components
 import { Home } from '../pages/Home'
 import { Detail } from "../pages/Detail";
 import { Nav } from "../components/Nav";
-import { NoAuth } from '../pages/NoAuth'
+import { NoAuth } from '../pages/NoAuth';
+//import ListOfFavorites from "../pages/ListOfFavorites";
+import { User } from "../pages/User";
+import { NotFound } from "../pages/NotFount";
 //styles
 import { GlobalStyles } from '../styles/globalStyles'
+
+const ListOfFavorites = React.lazy(() => import('../pages/ListOfFavorites'))
+
 
 
 function App(){
 
-    const {isAuth, setIsAuth} = useContext(Context);
+    const{
+        isAuth
+    } = useContext(Context);
 
     return(
-        <BrowserRouter>
-            <GlobalStyles/>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/pet/:id" element={<Home/>}/>
-                <Route path="/details/:id" element={<Detail/>}/>
+        <Suspense fallback={<div/>}>
+            <BrowserRouter>
+                <GlobalStyles/>
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/pet/:id" element={<Home/>}/>
+                    <Route path="/details/:id" element={<Detail/>}/>
 
-                {!!isAuth?
-                    <>
-                        <Route path="/liked" element={<p>Registrado</p>}/>
-                        <Route path="/user" element={<p>Registrado</p>}/>
-                    </>
-                    :
-                    <>
-                        <Route path="/liked" element={<NoAuth/>}/>
-                        <Route path="/user" element={<NoAuth/>}/>
-                    </>
-                }
+                    {!!isAuth?
+                        <>
+                            <Route path="/liked" element={<ListOfFavorites/>}/>
+                            <Route path="/user" element={<User/>}/>
+                        </>
+                        :
+                        <>
+                            <Route path="/liked" element={<NoAuth/>}/>
+                            <Route path="/user" element={<NoAuth/>}/>
+                        </>
+                    }
 
-                <Route path="/register" element={<NoAuth/>}/>
+                    <Route path="/register" element={<NoAuth/>}/>
 
-            </Routes>
-            <Nav/>
-        </BrowserRouter>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+                <Nav/>
+            </BrowserRouter>
+        </Suspense>
     )
 }
 
