a340d1e94339f42a742562a85821a8946ecab39c
JMG24a <jmg24a@gmail.com>
2022-03-10 16:06:39 -0400

    [UPDATE] entry para el manejo de jwt

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //graphql
-import { ApolloClient, ApolloProvider, InMemoryCache  } from '@apollo/client'
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink  } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 import { APIPhotoGql } from '../env'
 //components
-import { App } from './router/App'
-import { AuthProvider } from './context/context';
+import { App } from './router/App';
+import { AuthProvider } from './context';
+
+const httpLink = createHttpLink({
+    uri: APIPhotoGql
+})
+
+const authLink = setContext((_, { headers }) => {
+    const token = sessionStorage.getItem("token");
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
 
 const client = new ApolloClient({
-    uri: APIPhotoGql,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache()
 })
 
 ReactDOM.render(
- 
+
     <ApolloProvider client={client}>
         <AuthProvider>
             <App/>
@@ -21,4 +36,3 @@ ReactDOM.render(
     </ApolloProvider>,
     document.getElementById('app')
 )
-
